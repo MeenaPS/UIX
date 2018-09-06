d41349e10f403bb992c61a6bcb19e0d652b1a75b
MeenaPS <35720774+MeenaPS@users.noreply.github.com>
2018-09-06 17:12:00 +0530

    Update header.ts

--- header.ts
+++ header.ts
@@ -13,3 +13,32 @@ export class StaticHeaderComponent implements OnInit {
   }
 
 }
+
+
+import { Component, OnInit } from '@angular/core';
+import { Department } from '../model/department.model';
+
+@Component({
+  selector: 'app-models',
+  templateUrl: './models.component.html',
+  styleUrls: ['./models.component.css']
+})
+export class ModelsComponent implements OnInit {
+ model: any = {};
+
+ 
+ departments: Department[] =  [
+    {id:1, name:"Help Desk"},
+    {id:2, name:"Admin"},
+    {id:3, name:"HR"},
+    {id:4, name:"IT"}
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+}
+onSubmit() {
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.model))
+  }
+}
